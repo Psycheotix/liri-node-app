507345c57d304d76a9e33ce67b3e0586febc2048
CADeer <christinedeer@gmail.com>
2018-10-27 00:13:15 -0400

    some changes

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 #**LIRI Bot**
 
-####Liri is a *Language* Interpretation and Recognition Interface
+**Liri is a *Language* Interpretation and Recognition Interface**
 
 Liri will take the __*user's input*__ and return data requested.
 Different things you can ask Liri are Band Names, Song names and movie names and she will search in the universe of **OMDB**, **SPOTIFY** and **BANDS IN TOWN** to find information for you.
 
 ---
-
+[![asciicast](https://asciinema.org/a/9f9XVgSqzg4MC8EK1h4pBzdVW.png)](https://asciinema.org/a/9f9XVgSqzg4MC8EK1h4pBzdVW)
 
 
 
--- liri.js
+++ liri.js
@@ -10,52 +10,57 @@ var moment = require('moment')
 //console.log(spotify);
 
 
-console.log(Spotify)
+//console.log(Spotify)
  
 var action = process.argv[2];
  
 //*** ADDING EACH FUNCTION TO SWITCH ***////
 switch (action) {
-case "concertThis":
+case "concert-this":
 concertThis();
 break;
 
-case "spotifyThisSong":
+case "spotify-this-song":
 spotifyThisSong();
 break;
 
-case "movieThis":
+case "movie-this":
 movieThis();
 break;
 
-case "do_what_it_says":
-doWhatItSays();
+case "do-what-it-says":
+do_what_it_says();
 break;
 
 }
 
-//CONCERT-THIS FUNCTION
+// console.log(action)
+
+//CONCERT-THIS FUNCTION //
 function concertThis(){
     var userValue =  process.argv
     var artist = userValue.splice(3, userValue.length).join('');
-    console.log(artist, "this is our artist");
+    console.log("Concert information for: " + artist);
+
 
     request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", function (error, response, body) {
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:',JSON.parse(body)); // Print the HTML for the Google homepage.
+        // console.log('error:', error); // Print the error if one occurred
+        // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        // console.log('body:',JSON.parse(body)); // Print the HTML for the Google homepage.
         var results = JSON.parse(body);
-        console.log("Venue Name : " + results[0].venue.name );
+        console.log("Venue Name: " + results[0].venue.name );
         console.log("Venue Location: " + results[0].venue.city + ", " + results[0].venue.region)
         console.log("Concert Date: " + moment(results[0].datetime).format('MMMM Do YYYY, h:mm a'))
       });
 
 
 
-}
+};
 
-// /// SPOTIFY FUNCTION ///
 
+
+
+/// SPOTIFY FUNCTION ///
 function spotifyThisSong(){
     var userValue =  process.argv
     var song = userValue.splice(3, userValue.length).join(' ');
@@ -63,10 +68,10 @@ function spotifyThisSong(){
     spotify
     .search({ type: 'track', query: song, limit: 1 })
     .then(function(response) {
-    console.log("Song Name : " + response.tracks.items[0].name);
-    console.log("Artist Name : " + response.tracks.items[0].album.artists[0].name);
-    console.log("Album Name : " + response.tracks.items[0].album.name);
-    console.log("preview url : " + response.tracks.items[0].preview_url);
+    console.log("Song Name: " + response.tracks.items[0].name);
+    console.log("Artist Name: " + response.tracks.items[0].album.artists[0].name);
+    console.log("Album Name: " + response.tracks.items[0].album.name);
+    console.log("preview url: " + response.tracks.items[0].preview_url);
     })
     
     .catch(function(err) {
@@ -78,17 +83,19 @@ function spotifyThisSong(){
 };
 
 
-//MOVIE-THIS FUNCTION
+
+//MOVIE-THIS FUNCTION//
 function movieThis(){
     var userValue =  process.argv
     var movie = userValue.splice(3, userValue.length).join(' ');
 
     request("https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:',JSON.parse(body)); 
+        // console.log('error:', error); // Print the error if one occurred
+        // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        // console.log('body:',JSON.parse(body)); // Print the HTML for the Google homepage.
         var results = JSON.parse(body);
-        console.log(results)
+        // console.log(results)
+
 
     // * Title of the movie.
     console.log("Movie Title: " + results.Title);
@@ -103,8 +110,46 @@ function movieThis(){
     // * Language of the movie.
     console.log("Movie's Original Language: " + results.Language);
     // * Plot of the movie.
-    console.log("Movie's Plot: " + results.Plot);
+    console.log("Movie's Plot: " + results.Plot)
     // * Actors in the movie.
-    console.log("Actors in Movie: " + results.Actors);
+    console.log("Actors in Movie: " + results.Actors)
       });
+    
 };
+
+//do_what_it_says FUNCTION//
+function do_what_it_says(){
+    var fs = require("fs");
+    fs.readFile("random.txt", "utf8", function(error, data) {
+    
+        if (error) {
+            return console.log(error);
+          }
+        
+          var dataArr = data.split(",");
+          action = dataArr[0];
+          song = dataArr[1];
+          console.log(dataArr)
+          console.log(action)
+          
+          var spotify = new spotifyReq(keysFile.spotify);
+          spotify.search({ type: 'track', query: song}, function(error, data) {
+              if (error) {
+                  return console.log('Error occurred: ' + error);
+              }
+       //artist, song, preview link, album
+              console.log("The requested song is by " + data.tracks.items[0].artists[0].name);
+              console.log("The song is "+ data.tracks.items[0].name);
+              console.log("You can sample this song at " + data.tracks.items[0].preview_url);
+              console.log("This song is on the album " + data.tracks.items[0].album.name);     
+            
+        
+        
+     
+          
+        });
+    });
+}
+
+
+    
